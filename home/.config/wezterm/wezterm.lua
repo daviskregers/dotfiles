5ec839c92d2d660ceaf2a24b7d3f6a49ad143caf
Dāvis Krēgers <daviskregers@Daviss-MacBook-Pro.local>
2022-03-01 21:43:43 +0200

    add mac wezterm alt key

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -6,4 +6,7 @@ return {
   hide_tab_bar_if_only_one_tab = true,
   window_background_opacity = 0.9,
   font = wezterm.font("FiraCode Nerd Font Mono"),
+  send_composed_key_when_left_alt_is_pressed=true,
+  send_composed_key_when_right_alt_is_pressed=false,
+  use_ime=true
 }
