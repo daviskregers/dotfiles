eed8581dbf279191e4de3fb0e61962b6a99a8463
Dāvis Krēgers <daviskregers@protonmail.com>
2022-03-06 11:57:47 +0200

    switch to a light theme on wezterm, neovim

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,12 +1,12 @@
 local wezterm = require 'wezterm';
 
 return {
-  color_scheme = "deep",
+  color_scheme = "Alabaster",
   font_size = 10.0,
   hide_tab_bar_if_only_one_tab = true,
-  window_background_opacity = 0.9,
+  window_background_opacity = 0.8,
   font = wezterm.font("FiraCode Nerd Font Mono"),
   send_composed_key_when_left_alt_is_pressed=true,
   send_composed_key_when_right_alt_is_pressed=false,
-  use_ime=true
+  use_ime=true,
 }
