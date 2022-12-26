8c65aea9c2e2e65f5a45634a57196ba27e14a365
Dāvis Krēgers <daviskregers@protonmail.com>
2022-12-26 16:50:16 +0200

    feat: wezterm transparency and remove padding

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require 'wezterm';
 
-local arch = io.popen('uname -s','r'):read('*l')
+local arch = io.popen('uname -s', 'r'):read('*l')
 
 local font_size = 10.0
 if arch == 'Darwin' then
@@ -8,15 +8,22 @@ if arch == 'Darwin' then
 end
 
 return {
-  color_scheme = "Afterglow",
-  colors = {
-    background = "#282828",
-  },
-  font_size = font_size,
-  hide_tab_bar_if_only_one_tab = true,
-  window_background_opacity = 1.0,
-  font = wezterm.font("FiraCode Nerd Font Mono"),
-  send_composed_key_when_left_alt_is_pressed=true,
-  send_composed_key_when_right_alt_is_pressed=false,
-  use_ime=true,
+    color_scheme = "Afterglow",
+    colors = {
+        background = "#282828",
+    },
+    font_size = font_size,
+    hide_tab_bar_if_only_one_tab = true,
+    window_background_opacity = 0.9,
+    font = wezterm.font("FiraCode Nerd Font Mono"),
+    send_composed_key_when_left_alt_is_pressed = true,
+    send_composed_key_when_right_alt_is_pressed = false,
+    use_ime = true,
+    enable_scroll_bar = false,
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    },
 }
