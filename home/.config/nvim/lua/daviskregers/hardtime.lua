34d936d7c0b180d423654fc09aa5a83b4fe61fe5
Dāvis Krēgers <daviskregers@protonmail.com>
2023-04-05 12:35:41 +0300

    chore: disable hardtime

--- home/.config/nvim/lua/daviskregers/hardtime.lua
+++ home/.config/nvim/lua/daviskregers/hardtime.lua
@@ -1,16 +1,10 @@
 -- https://github.com/takac/vim-hardtime
 
 vim.cmd [[
-    let g:hardtime_default_on = 1
+    let g:hardtime_default_on = 0
     let g:list_of_normal_keys = ["h", "j", "k", "l", "-", "+", "<UP>", "<DOWN>", "<LEFT>", "<RIGHT>"]
     let g:list_of_visual_keys = ["h", "j", "k", "l", "-", "+", "<UP>", "<DOWN>", "<LEFT>", "<RIGHT>"]
     let g:list_of_insert_keys = ["<UP>", "<DOWN>", "<LEFT>", "<RIGHT>"]
     let g:list_of_disabled_keys = []
     let g:hardtime_showmsg = 1
 ]]
--- vim.g.hardtime_default_on = 1
--- vim.g.list_of_normal_keys = {"h", "j", "k", "l", "-", "+", "<UP>", "<DOWN>", "<LEFT>", "<RIGHT>"}
--- vim.g.list_of_visual_keys = {"h", "j", "k", "l", "-", "+", "<UP>", "<DOWN>", "<LEFT>", "<RIGHT>"}
--- vim.g.list_of_insert_keys = {"<UP>", "<DOWN>", "<LEFT>", "<RIGHT>"}
--- vim.g.list_of_disabled_keys = {}
--- vim.g.hardtime_showmsg = 1
