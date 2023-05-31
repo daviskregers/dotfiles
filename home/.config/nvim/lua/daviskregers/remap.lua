4cc246099ff8c4bef5fad1516574f3a79a958981
Dāvis Krēgers <daviskregers@protonmail.com>
2023-05-31 20:46:16 +0300

    feat: add diagnostics popup to nvim

--- home/.config/nvim/lua/daviskregers/remap.lua
+++ home/.config/nvim/lua/daviskregers/remap.lua
@@ -51,3 +51,18 @@ vim.keymap.set("v", "gs", ":sort u<CR>")
 
 -- align
 vim.keymap.set("v", "<leader>=", ":'<,'>Align ")
+
+-- show diagnostics popup
+_G.LspDiagnosticsPopupHandler = function()
+  print('called')
+  local current_cursor = vim.api.nvim_win_get_cursor(0)
+  local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or {nil, nil}
+
+  -- Show the popup diagnostics window,
+  -- but only once for the current cursor location (unless moved afterwards).
+  if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
+    vim.w.lsp_diagnostics_last_cursor = current_cursor
+    vim.diagnostic.open_float(0, {scope="cursor"})
+  end
+end
+vim.keymap.set("n", "<leader>k", _G.LspDiagnosticsPopupHandler)
