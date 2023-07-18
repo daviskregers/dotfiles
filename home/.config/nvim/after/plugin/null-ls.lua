97408d650572073ac6cf4a0b521a56e2723468c2
Dāvis Krēgers <daviskregers@protonmail.com>
2023-07-18 14:02:26 +0300

    fix: lsp config

--- home/.config/nvim/after/plugin/null-ls.lua
+++ home/.config/nvim/after/plugin/null-ls.lua
@@ -18,7 +18,6 @@ end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/
 local code_actions = null_ls.builtins.code_actions
-local completion = null_ls.builtins.completion
 local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 
