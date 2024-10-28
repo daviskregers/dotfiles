8fc556554d2f20c03351c5eccc7d051656b196c9
Dāvis Krēgers <daviskregers@protonmail.com>
2024-10-28 11:27:42 +0200

    feat(nvim): project based configs, trouble autopen based on it

--- home/.config/nvim/lua/plugins/trouble.lua
+++ home/.config/nvim/lua/plugins/trouble.lua
@@ -1,11 +1,24 @@
 return {
     "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        auto_close = true,
-        auto_preview = true,
-        modes = {
-            diagnostics = { auto_open = true },
+    event = "VeryLazy",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "klen/nvim-config-local",
+    },
+    config = function()
+        -- if vim.g.TROUBLE_AUTO_OPEN == nil then
+        --     print('TROUBLE_AUTO_OPEN is not set, defaulting to true')
+        --     vim.g.TROUBLE_AUTO_OPEN = true
+        -- end
+
+        print('Trouble auto open:', vim.g.TROUBLE_AUTO_OPEN)
+
+        require('trouble').setup {
+            auto_close = true,
+            auto_preview = true,
+            modes = {
+                diagnostics = { auto_open = vim.g.TROUBLE_AUTO_OPEN },
+            }
         }
-    }
+    end
 }
