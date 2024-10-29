8bd177fb727fec63067f6f5ee49dfee0177fd661
Dāvis Krēgers <daviskregers@protonmail.com>
2024-10-29 20:34:27 +0200

    feat(nvim): improved overrideable terminal commands

--- home/.config/nvim/lua/plugins/term.lua
+++ home/.config/nvim/lua/plugins/term.lua
@@ -30,86 +30,94 @@ return {
 
         vim.keymap.set("n", "<leader>gg", function() lazygit:toggle() end, { desc = 'git - toggle laztgit' })
 
-        -- typescript
-        vim.api.nvim_create_augroup('tests', { clear = true })
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'typescript',
-            command = 'nnoremap <buffer><silent><leader>tl :vsplit term://pnpm test<cr>'
-        })
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'typescript',
-            command = 'nnoremap <buffer><silent><leader>tk :split term://pnpm test<cr>'
-        })
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'typescript',
-            command = 'nnoremap <buffer><silent><leader>tg :vsplit term://pnpm lint<cr>'
-        })
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'typescript',
-            command = "nnoremap <buffer><silent><leader>tw :vsplit term://nodemon --exec 'clear && pnpm test' -e ts,js<cr>"
-        })
-        -- javascript
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'javascript',
-            command = 'nnoremap <buffer><silent><leader>tl :vsplit term://pnpm test<cr>'
-        })
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'javascript',
-            command = 'nnoremap <buffer><silent><leader>tl :split term://pnpm test<cr>'
-        })
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'javascript',
-            command = 'nnoremap <buffer><silent><leader>tg :vsplit term://pnpm lint<cr>'
-        })
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'javascript',
-            command = "nnoremap <buffer><silent><leader>tw :vsplit term://nodemon --exec 'clear && pnpm test' -e ts,js<cr>"
-        })
-        -- php
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'php',
-            command = 'nnoremap <buffer><silent><leader>tl :vsplit term://composer test %<cr>'
-        })
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'php',
-            command = 'nnoremap <buffer><silent><leader>tk :split term://composer test %<cr>'
-        })
-        -- php
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'dockerfile',
-            command = 'nnoremap <buffer><silent><leader>tl :vsplit term://docker build .<cr>'
-        })
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'dockerfile',
-            command = 'nnoremap <buffer><silent><leader>tk :split term://docker build .<cr>'
-        })
-        -- golang
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'go',
-            command = 'nnoremap <buffer><silent><leader>tr :vsplit term://go run .<cr>'
-        })
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'go',
-            command = 'nnoremap <buffer><silent><leader>tl :vsplit term://go test<cr>'
-        })
-        vim.api.nvim_create_autocmd('FileType', {
-            group = 'tests',
-            pattern = 'go',
-            command = 'nnoremap <buffer><silent><leader>tk :split term://go test<cr>'
+
+        local defaultTestCommands = {
+            typescript = {
+                test = "pnpm test",
+                lint = "pnpm lint",
+                watch = "nodemon --exec 'clear && pnpm test' -e ts,js"
+            },
+            javascript = {
+                test = "pnpm test",
+                lint = "pnpm lint",
+                watch = "nodemon --exec 'clear && pnpm test' -e ts,js"
+            },
+            php = {
+                test = "composer test %"
+            },
+            dockerfile = {
+                test = "docker build ."
+            },
+            go = {
+                test = "go test",
+                run = "go run .",
+            }
+        }
+
+        -- vim.api.nvim_create_autocmd('User', {
+        --     pattern = '*',
+        --     callback = function(ev)
+        --         print(string.format('event fired: %s', vim.inspect(ev)))
+        --     end
+        -- })
+        local COMMAND_ARGS = {}
+
+        vim.api.nvim_create_autocmd('User', {
+            pattern = 'ConfigLocalFinished',
+            callback = function()
+                if CONFIG_TEST_COMMANDS ~= nil then
+                    print('Overriding test commands')
+                    print(vim.inspect(CONFIG_TEST_COMMANDS))
+                    for filetype, commands in pairs(CONFIG_TEST_COMMANDS) do
+                        print(string.format('Overriding test commands for %s', filetype))
+                        print(vim.inspect(commands))
+                        for command, value in pairs(commands) do
+                            print(string.format('[%s] Overriding %s with %s', filetype, command, value))
+                            if defaultTestCommands[filetype] == nil then
+                                defaultTestCommands[filetype] = {}
+                            end
+                            defaultTestCommands[filetype][command] = value
+                        end
+                    end
+                end
+
+                -- print(vim.inspect(defaultTestCommands))
+
+                local types = {
+                    { "l", "test" },
+                    { "g", "lint" },
+                    { "w", "watch" },
+                    { "r", "run" },
+                }
+
+                for filetype, commands in pairs(defaultTestCommands) do
+                    for _, type in ipairs(types) do
+                        local key, command = unpack(type)
+                        vim.api.nvim_create_autocmd('FileType', {
+                            pattern = filetype,
+                            callback = function()
+                                local argKey = filetype .. ':::' .. command
+                                if COMMAND_ARGS[argKey] == nil then
+                                    COMMAND_ARGS[argKey] = ''
+                                end
+
+                                vim.keymap.set("n", "<leader>t" .. key, function()
+                                    if command[command] == nil then
+                                        print(string.format('No command found for %s', command))
+                                        return
+                                    end
+                                    vim.cmd('vsplit')
+                                    vim.cmd(string.format('term %s %s', commands[command], COMMAND_ARGS[argKey]))
+                                end)
+
+                                vim.keymap.set("n", "<leader>t" .. string.upper(key), function()
+                                    COMMAND_ARGS[argKey] = vim.fn.input('Enter additional args (' .. command .. '): ', COMMAND_ARGS[argKey])
+                                end)
+                            end
+                        })
+                    end
+                end
+            end
         })
     end
 }
--- home/.config/nvim/lua/plugins/trouble.lua
+++ home/.config/nvim/lua/plugins/trouble.lua
@@ -6,18 +6,17 @@ return {
         "klen/nvim-config-local",
     },
     config = function()
-        -- if vim.g.TROUBLE_AUTO_OPEN == nil then
-        --     print('TROUBLE_AUTO_OPEN is not set, defaulting to true')
-        --     vim.g.TROUBLE_AUTO_OPEN = true
-        -- end
+        if CONFIG_TROUBLE_AUTO_OPEN == nil then
+            CONFIG_TROUBLE_AUTO_OPEN = true
+        end
 
-        print('Trouble auto open:', vim.g.TROUBLE_AUTO_OPEN)
+        print('Trouble auto open:', CONFIG_TROUBLE_AUTO_OPEN)
 
         require('trouble').setup {
             auto_close = true,
             auto_preview = true,
             modes = {
-                diagnostics = { auto_open = vim.g.TROUBLE_AUTO_OPEN },
+                diagnostics = { auto_open = CONFIG_TROUBLE_AUTO_OPEN },
             }
         }
     end
