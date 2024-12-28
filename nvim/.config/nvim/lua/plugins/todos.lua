f4261e222bedde782e5c6ff25a9185ce72fe430e
Dāvis Krēgers <daviskregers@protonmail.com>
2024-12-29 00:53:20 +0200

    feat(nvim): keybind for opening todos in quickfixlist

--- nvim/.config/nvim/lua/plugins/todos.lua
+++ nvim/.config/nvim/lua/plugins/todos.lua
@@ -2,5 +2,8 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
+    },
+    keys = {
+        { '<leader>pd', ":TodoQuickFix<CR>", mode = 'n', desc = 'TODOs' }
     }
 }
