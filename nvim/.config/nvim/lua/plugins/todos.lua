5dd31b795f9bae25fc4b28c92c7bfff34610e516
Dāvis Krēgers <daviskregers@protonmail.com>
2024-12-29 22:03:51 +0200

    fix(nvim): swap todo binding to telescope instead of quickfixlist

--- nvim/.config/nvim/lua/plugins/todos.lua
+++ nvim/.config/nvim/lua/plugins/todos.lua
@@ -4,6 +4,6 @@ return {
     opts = {
     },
     keys = {
-        { '<leader>pd', ":TodoQuickFix<CR>", mode = 'n', desc = 'TODOs' }
+        { '<leader>pd', ":TodoTelescope<CR>", mode = 'n', desc = 'TODOs' }
     }
 }
