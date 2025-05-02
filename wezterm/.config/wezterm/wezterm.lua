3754992dbb31044f64ea302958fe83a515e546c7
Dāvis Krēgers <daviskregers@protonmail.com>
2025-05-02 10:11:26 +0300

    fix(wezterm): emojis

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -15,7 +15,11 @@ return {
     font_size = font_size,
     hide_tab_bar_if_only_one_tab = true,
     window_background_opacity = 0.9,
-    font = wezterm.font("FiraCode Nerd Font Mono"),
+    font = wezterm.font_with_fallback({
+        "FiraCode Nerd Font Mono",
+        "Apple Color Emoji",
+        "OpenMoji Color",
+    }),
     send_composed_key_when_left_alt_is_pressed = true,
     send_composed_key_when_right_alt_is_pressed = false,
     use_ime = true,
