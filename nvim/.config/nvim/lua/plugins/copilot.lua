74f0e93ddc46b28e879c367ed1bba2211af5c6ba
Dāvis Krēgers <daviskregers@protonmail.com>
2026-01-14 17:01:41 +0200

    fix(nvim): reenable copilot

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -29,7 +29,7 @@ return {
                 end,
                 suggestion = {
                     enabled = true,
-                    auto_trigger = false,
+                    auto_trigger = true,
                     hide_during_completion = true,
                     debounce = 75,
                     trigger_on_accept = true,
