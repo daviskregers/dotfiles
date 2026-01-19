ac4b3ecab3c8d57424eb1b1a91ce57c16beb1521
Dāvis Krēgers <daviskregers@protonmail.com>
2026-01-19 14:04:40 +0200

    feat(nvim): insure treesitter has http installed

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
         build = ":TSUpdate",
         config = function()
             require 'nvim-treesitter.configs'.setup {
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python" },
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "http"},
                 sync_install = false,
                 ignore_install = {},
                 auto_install = false,
