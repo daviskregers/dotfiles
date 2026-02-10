838653601b78ad17dfc6eb93cac8f911da4e73a0
Dāvis Krēgers <daviskregers@protonmail.com>
2026-02-10 13:32:49 +0200

    feat(nvim): add lazydocker

---
+++ nvim/.config/nvim/lua/plugins/docker.lua
@@ -0,0 +1,32 @@
+-- brew install lazydocker
+-- yay -S lazydocker
+return {
+    {
+      "mgierada/lazydocker.nvim",
+      dependencies = { "akinsho/toggleterm.nvim" },
+      config = function()
+        require("lazydocker").setup({
+            border = "curved", -- valid options are "single" | "double" | "shadow" | "curved"
+            width = 0.9, -- width of the floating window (0-1 for percentage, >1 for absolute columns)
+            height = 0.9, -- height of the floating window (0-1 for percentage, >1 for absolute rows)
+        })
+      end,
+      event = "BufRead",
+      keys = {
+        {
+          "<leader>du",
+          function()
+            require("lazydocker").open()
+          end,
+          desc = "Open Lazydocker floating window",
+        },
+        {
+          "<leader>dc",
+          function()
+            vim.cmd[[!docker builder prune -af]]
+          end,
+          desc = "Prune docker builder",
+        },
+      },
+    },
+}
