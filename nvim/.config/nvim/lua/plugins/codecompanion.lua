91f3fee8ac6521f4c06736a50a1f9d884245dac6
Dāvis Krēgers <daviskregers@protonmail.com>
2026-03-17 14:30:08 +0200

    feat(nvim): better codecompanion

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -1,21 +1,16 @@
 return {
     {
         "olimorris/codecompanion.nvim",
+        version = "^19.0.0",
+        opts = {},
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-treesitter/nvim-treesitter",
         },
-        opts = {
-            opts = {
-              log_level = "DEBUG", -- or "TRACE"
-            },
-        },
         keys = {
-            { '<leader>cc', ":CodeCompanion<cr>", mode = 'n', desc = 'Code Companion' },
-            { '<leader>cc', ":'<,'>CodeCompanion<cr>", mode = 'v', desc = 'Code Companion' },
-            { '<leader>ca', ":CodeCompanionActions<cr>", mode = 'n', desc = 'Code Companion Actions' },
-            { '<leader>ch', ":CodeCompanionChat<cr>", mode = 'n', desc = 'Code Companion Chat' },
-            { '<leader>cm', ":CodeCompanionCmd", mode = 'n', desc = 'Code Companion Command' },
-        }
+            { "<C-\\>", ":CodeCompanion<CR>", mode = "n", desc = "Run CodeCompanion" },
+            { "<C-\\>", ":CodeCompanion<CR>", mode = "v", desc = "Run CodeCompanion on selection" },
+            { "<C-_>", ":CodeCompanionChat<CR>", mode = "n", desc = "Toggle CodeCompanion Chat" },
+        },
     },
 }
